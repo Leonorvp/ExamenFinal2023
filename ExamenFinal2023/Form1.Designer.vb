c9e6ad514bd99a357c54ea44c2f2d39de0bbb00d
Leonor Valenzuela <silvialeovp12@gmail.com>
2023-10-13 11:36:46 -0600

    LOGING-1.0

--- ExamenFinal2023/Form1.Designer.vb
+++ ExamenFinal2023/Form1.Designer.vb
@@ -22,10 +22,81 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(358, 150)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(358, 239)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.ForeColor = SystemColors.ButtonHighlight
+        Label1.Location = New Point(251, 150)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 25)
+        Label1.TabIndex = 2
+        Label1.Text = "Usuario:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.ForeColor = SystemColors.ButtonHighlight
+        Label2.Location = New Point(235, 242)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(117, 25)
+        Label2.TabIndex = 3
+        Label2.Text = "Contraseña:"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Arial", 10F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Location = New Point(316, 347)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(132, 40)
+        Button1.TabIndex = 4
+        Button1.Text = "ACCEDER"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Highlight
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "LOGIN"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
 End Class
